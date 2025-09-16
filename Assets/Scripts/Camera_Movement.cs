0d58b51d2e58db9b181e7ab3f3853aed14ab6cde
H33O <rivals.hugo@gmail.com>
2025-09-16 18:26:46 +0200

    [HRI] fin de cours

--- Assets/Scripts/Camera_Movement.cs
+++ Assets/Scripts/Camera_Movement.cs
@@ -1,23 +1,32 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Camera_Movement : MonoBehaviour
 {
+   
     private float _horizontalMovement;
     private float _verticalMovement;
-    private Vector3 _movement;
-    [SerializeField] private float speed; 
-    void Start()
-    {
+    private Vector3 _rotation;
+    [SerializeField]  private float _speed = 2f;
+   
+    
+       
         
-    }
+        
+    
 
-    // Update is called once per frame
     void Update()
     {
         _horizontalMovement = Input.GetAxis("Horizontal");
         _verticalMovement = Input.GetAxis("Vertical");
-        _movement = new Vector3(_horizontalMovement, 0, _verticalMovement);
-        _movement.Normalize();
-        _movement *= speed;
+        _rotation = new Vector3(_horizontalMovement, 0f, _verticalMovement);
+        _rotation.Normalize();
+        _rotation *= _speed;
+        
+        
+        
+        
+          
     }
+    
 }
